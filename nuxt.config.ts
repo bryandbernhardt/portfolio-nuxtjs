23c7a53f9fcade7d90714bc5ffef873ad8f5b278
Bryan Dietrich Bernhardt <bryan@minhavisita.com.br>
2023-10-18 12:36:59 -0300

    chore(app): adicionar fontaine

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
     '@nuxtjs/color-mode',
+    '@nuxtjs/fontaine',
     '@nuxt/image',
     'nuxt-purgecss',
     'nuxt-icon',
