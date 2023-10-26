ae1ebadd5aa508c2766c5e335e967209eca847eb
Bryan Dietrich Bernhardt <bryan@minhavisita.com.br>
2023-10-26 05:14:40 -0300

    chore(app): adicionar link completo hreflang

--- app.vue
+++ app.vue
@@ -33,7 +33,7 @@ useSeoMeta({
       <Head>
         <Title>{{ title }}</Title>
         <template v-for="link in head.link" :key="link.id">
-          <Link :id="link.id" :rel="link.rel" :href="link.href" :hreflang="link.hreflang" />
+          <Link :id="link.id" :rel="link.rel" :href="'https://www.bryandbernhardt.com' + link.href" :hreflang="link.hreflang" />
         </template>
         <template v-for="meta in head.meta" :key="meta.id">
           <Meta :id="meta.id" :property="meta.property" :content="meta.content" />
